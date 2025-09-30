5d3c6314beebe4054066f5c41d7cd8a6dc3a1bca
development0264 <development0246@gmail.com>
2025-09-30 15:02:44 +0530

    updated index

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "887822631"
+    serviceWorkerVersion: "1683413795"
   }
 });
