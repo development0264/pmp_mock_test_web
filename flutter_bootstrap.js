2991a30b64442143e04197c0d09545fc1735910f
development0264 <development0246@gmail.com>
2025-10-02 18:38:11 +0530

    change not reflected

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2640405974"
+    serviceWorkerVersion: "3011431797"
   }
 });
